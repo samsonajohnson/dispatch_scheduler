6a138ce521b56cccc483fff5f15cbdb299f94cc6
samsonajohnson <samson.a.johnson@gmail.com>
2016-06-01 10:46:37 -0400

    Working on making a telescope class, and making a utils file

---
+++ telescope.py
@@ -0,0 +1,34 @@
+"""
+a class for a telescope in use with the dispatch scheduler. need to keep track 
+of where it is pointing, etc. Could also implement two observing mode feature.
+"""
+
+from configobj import ConfigObj
+import utils
+import ephem
+import datetime
+
+
+class telescope:
+    def __init__(self):
+        self.load_config()
+
+    def load_config(self):
+        try:
+            config = ConfigObj(self.base_directory+'/config/'+self.config_file)
+            self.slewrate = float(config['Setup']['SLEWRATE'])
+            self.alt = float(config['Setup']['PARK_ALT'])
+            self.azm = float(config['Setup']['PARK_AZM'])
+        except:
+            print('ERROR accessing configuration file: ' + self.config_file)
+            sys.exit()
+    
+
+    def acquire_target(self,target):
+        """
+        slew to a given target, give some overhead for actually acquiring?
+        """
+
+    def slew(self,target_alt,target_azm,time):
+        
+        
---
+++ utils.py
@@ -0,0 +1,11 @@
+"""
+a compilation of multi use funtions
+"""
+import math
+
+
+def degress(radians):
+    return radians*180./math.pi
+
+def radians(degrees):
+    return degrees*math.pi/180.
